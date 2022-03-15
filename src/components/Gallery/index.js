993e4196519d9ba64ef6a956049ca88d1fca6ec4
Kenia Lizama-Gamez <ke.yulizama@gmail.com>
2022-03-15 16:48:09 -0400

    gallery implemented

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,10 +1,29 @@
 import React from "react";
+// capitalize name value when it's rendered
+import { capitalizeFirstLetter } from '../../utils/helpers';
+// import images for displaying
+import photo from "../../assets/small/commercial/0.jpg";
 
 function Gallery() {
+
+    const currentCategory = {
+        
+        name: "commercial",
+        description: "Photos of grocery stores, food trucks, and other commercial projects",
+        
+    };
+
     return(
         <section>
-            <h1>NAME</h1>
-            <p>DESCRFIPTION</p>
+            <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
+            <p>{currentCategory.name}</p>
+            <div>
+                <img
+                src={photo}
+                alt="Commercial Example"
+                className="img-thumbnail mx-1"
+                />
+            </div>
         </section>
     );
 }
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,38 +1,25 @@
 import React from "react";
+import { capitalizeFirstLetter } from "../../utils/helpers"; 
+import React, { useEffect } from 'react';
 
+function Nav (props) {
+    // useEffect hook to re-render when the value updates
+    useEffect(() => {
+        document.title = capitalizeFirstLetter(currentCategory.name);
+    }, [currentCategory]);
 
-function Nav () {
-    const categories = [
-        {
-            name: "commercial",
-            description:
-            "Photos of grocery stores, food trucks, and other commercial projects",
-        },
-        {
-            name: "portraits",
-            description: "Portraits of people in my life"
-        },
-        {
-            name: "food",
-            description: "Delicious delicacies"
-        },
-        {
-            name: "landscape",
-            description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-        },
-    ];
-
-    // onClick needs to be defined
-    function categorySelected() {
-        console.log('clicked')
-    }
+    const {
+        categories = [],
+        setCurrentCategory,
+        currentCategory,
+    } = props;
 
     return (
-        <header>
+        <header className="flex-row px-1">
             <h2>
                 {/* add attribute specific for testing to the <a> element */}
                 <a data-testid="link" href="/">
-                    <span role="img" aria-label="camera">📸</span>Oh Snap!
+                    <span role="img" aria-label="camera">{" "}📸</span>{" "}Oh Snap!
                 </a>
             </h2>
             <nav>
@@ -47,10 +34,16 @@ function Nav () {
 
                     {/* map over the array */}
                     {categories.map((category) => (
-                        <li className="mx-1" key={category.name}>
+                        <li 
+                        className={`mx-1 ${currentCategory.name === category.name && 'navActive'}`}
+                        key={category.name}
+                        >
                     {/* use onClick to set up event listener for when a category is clicked */}
-                            <span onClick={categorySelected}>
-                                {category.name}
+                            <span 
+                            onClick={() => {
+                                setCurrentCategory(category)
+                            }}>
+                                {capitalizeFirstLetter(category.name)}
                             </span>
                         </li>
                     ))}
